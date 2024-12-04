89d1bd46e2574e8c76867a1fcc15f2cf2f7b6ff2
Lukas Winkler <lukas.winkler08@htl-salzburg.ac.at>
2024-12-04 20:18:25 +0100

    Students erstellt, Hintergrund entfernt, Chair Spawning

--- Assets/MyGame/Scripts/RoomManager.cs
+++ Assets/MyGame/Scripts/RoomManager.cs
@@ -1,11 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Security.Principal;
 using UnityEngine;
 
 public class RoomManager : MonoBehaviour
 {
-
     [SerializeField] private TableLayoutData tableLayout; //Ref zu TableLayout ScriptableObject
     [SerializeField] private StudentData[] students; //
     [SerializeField] private GameObject tablePrefab; //Prefab für Tisch
@@ -14,21 +12,27 @@ public class RoomManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        for(int row = 0; row <tableLayout.rows; row++)
+        for (int row = 0; row < tableLayout.rows; row++)
         {
-            for(int col = 0; col <tableLayout.columns; col++)
+            for (int col = 0; col < tableLayout.columns; col++)
             {
                 Vector3 tablePosition = new Vector3(col * tableLayout.tableSpacing, 0, row * tableLayout.tableSpacing);
                 GameObject table = Instantiate(tablePrefab, tablePosition, Quaternion.identity, transform);
 
+                // Stühle platzieren
+                Transform pos1 = table.transform.Find("Position1");
+                Transform pos2 = table.transform.Find("Position2");
+
+                if (pos1)
+                {
+                    GameObject chair = Instantiate(chairPrefab, pos1.position, pos1.rotation, table.transform);
+                }
+
+                if (pos2)
+                {
+                    GameObject chair = Instantiate(chairPrefab, pos2.position, pos2.rotation, table.transform);
+                }
             }
         }
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
